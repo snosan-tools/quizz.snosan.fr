248b7c612704ad094aca5dc893f824d1b2a1def2
Antoine Augusti <antoine.augusti@developpement-durable.gouv.fr>
2018-07-13 22:11:28 +0200

    Present good or bad answer

--- src/components/Question.vue
+++ src/components/Question.vue
@@ -6,18 +6,41 @@
     <strong>{{ question.text }}</strong>
 
     <div class="mt-5">
-      <div v-if="question.type === 'tf'">
-        <input type="radio" name="currentQuestion" id="trueAnswer" v-model="answer" value="t" class="mb-4"><label for="trueAnswer" class="mx-2">Vrai</label><br/>
-        <input type="radio" name="currentQuestion" id="falseAnswer" v-model="answer" value="f"><label for="falseAnswer" class="mx-2">Faux</label><br/>
-      </div>
-
-      <div v-if="question.type === 'mc'">
-        <div v-for="(mcanswer, index) in question.answers" :key='index'>
-        <input type="radio" :id="'answer'+index" name="currentQuestion" v-model="answer" :value="mcanswer" class="mb-4"><label :for="'answer'+index" class="mx-2">{{mcanswer}}</label><br/>
+      <div v-if="!resultStage">
+        <div v-if="question.type === 'tf'">
+          <input type="radio" name="currentQuestion" id="trueAnswer" v-model="answer" value="t" class="mb-4"><label for="trueAnswer" class="mx-2">Vrai</label><br/>
+          <input type="radio" name="currentQuestion" id="falseAnswer" v-model="answer" value="f"><label for="falseAnswer" class="mx-2">Faux</label><br/>
         </div>
+
+        <div v-if="question.type === 'mc'">
+          <div v-for="(mcanswer, index) in question.answers" :key='index'>
+          <input type="radio" :id="'answer'+index" name="currentQuestion" v-model="answer" :value="mcanswer" class="mb-4"><label :for="'answer'+index" class="mx-2">{{mcanswer}}</label><br/>
+          </div>
+        </div>
+
+        <button
+          @click="checkAnswer"
+          class="bg-blue hover:bg-blue-dark text-white font-bold py-2 px-4 rounded mt-8">
+          Continuer
+        </button>
       </div>
 
-      <button @click="submitAnswer" class="bg-blue hover:bg-blue-dark text-white font-bold py-2 px-4 rounded mt-10">Répondre</button>
+      <div v-if="resultStage">
+        <div v-if="answerIsGood" class="text-green mb-5">
+          Bonne réponse !
+        </div>
+        <div v-else class="text-red mb-5">
+          Oups, pas tout à fait.
+        </div>
+
+        La bonne réponse est : {{ presentedAnswer }}.
+
+        <button
+          @click="submitAnswer"
+          class="bg-blue hover:bg-blue-dark text-white font-bold py-2 px-4 rounded mt-8">
+          Continuer
+        </button>
+      </div>
     </div>
   </div>
 </template>
@@ -28,13 +51,29 @@ export default {
   props: ['question', 'question-number', 'nbQuestions'],
   data () {
     return {
-      answer: ''
+      answer: '',
+      resultStage: false
     }
   },
   methods: {
     submitAnswer: function () {
       this.$emit('answer', {answer: this.answer})
       this.answer = null
+      this.resultStage = false
+    },
+    checkAnswer: function () {
+      this.resultStage = true
+    }
+  },
+  computed: {
+    answerIsGood: function () {
+      return this.answer === this.question.answer
+    },
+    presentedAnswer: function () {
+      if (this.question.type === 'tf') {
+        return (this.question.answer === 't') ? 'Vrai' : 'Faux'
+      }
+      return this.question.answer
     }
   }
 }
